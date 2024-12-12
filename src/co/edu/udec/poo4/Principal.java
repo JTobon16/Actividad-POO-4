78a13f4b02113213a6989be3544b7520514789ed
altoc <altoc@Jonathan-Tobon>
2024-12-12 03:12:30 -0500

    esta es mi main principal

---
+++ src/co/edu/udec/poo4/Principal.java
@@ -0,0 +1,20 @@
+package co.edu.udec.poo4;
+import co.edu.udec.poo4.Ventanas.VentanaPrincipal;
+
+public class Principal {
+
+    
+    
+    
+    public static void main(String[] args) {
+      
+            VentanaPrincipal ventana = new  VentanaPrincipal();
+            
+            //ventana.setLocationRelativeTo(null);
+            ventana.setExtendedState(VentanaPrincipal.MAXIMIZED_BOTH);
+            ventana.setVisible(true);
+           
+            
+    
+    } 
+}
